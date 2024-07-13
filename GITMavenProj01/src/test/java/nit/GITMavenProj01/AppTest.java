c855571d788aa4fe62c626202035597b2c72a52b
pavan <pavan@pavan>
2024-07-13 19:23:20 +0530

    initial project commit

---
+++ GITMavenProj01/src/test/java/nit/GITMavenProj01/AppTest.java
@@ -0,0 +1,18 @@
+package nit.GITMavenProj01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
